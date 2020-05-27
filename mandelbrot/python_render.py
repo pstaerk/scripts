af85020f31c4b32df7b67b92dc3d87c4bcf88d8f
pstaerk <philipp.staerk@t-online.de>
2020-05-27 17:08:45 +0200

    Added cli functionality

--- mandelbrot/python_render.py
+++ mandelbrot/python_render.py
@@ -1,15 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import click
 
-def main():
+@click.command()
+@click.option('-i', default='Render0.out')
+def render_file(i):
     """Render a image from the mandelbrot cpp for testing.
     """
     pic = np.empty((2*500, 2*1000))
-    with open('./Myfile2.txt') as f:
+    with open(i) as f:
         for i, l in enumerate(f):
             pic[:, i] = l.split(';')[:-1]
     plt.matshow(pic, cmap=plt.get_cmap('cividis'))
     plt.show()
+
+def main():
+    """Main code.
+
+    """
+    render_file()
 
 if __name__ == "__main__":
     main()
